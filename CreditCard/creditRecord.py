b324689cdcc2eb7a87bd9b89c511e30b47fb0a10
“WangHungHsun” <frank53530@gmail.com>
2023-07-08 16:12:49 +0800

    rename personalData

--- CreditCard/creditRecord.py
+++ CreditCard/creditRecord.py
@@ -1,13 +1,9 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn import preprocessing
 
 
-df = pd.read_csv("D:/期中專題/creditcard./credit_record.csv")
+
+df = pd.read_csv("D:/期中專題/BDSE31TeamSix/CreditCard/credit_record.csv")
 print(df.describe)
 total = df["STATUS"].count()
 c = df["STATUS"].value_counts()['C']
