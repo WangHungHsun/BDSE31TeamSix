e0965c6807ebee4f042cab80d4b8d66d3ddb1669
WangHungHsun <134590959+WangHungHsun@users.noreply.github.com>
2023-08-27 23:27:24 +0800

    update flask scoring

--- front_end/flask_test.py
+++ front_end/flask_test.py
@@ -2,10 +2,7 @@ from flask import Flask, render_template, request, url_for
 from datetime import datetime
 from pathlib import Path
 import uuid
-
-# practice start
-# practice end
-
+import json
 
 app = Flask(__name__)
 
@@ -20,9 +17,45 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", page_header="page_header")
 
-@app.route("/form")
+
+
+
+
+@app.route('/form', methods=['GET', 'POST'])
 def form():
-    return render_template("form.html", page_header="page_header")
+    if request.method == "GET":
+       return render_template("form.html", page_header="page_header")
+    elif request.method == "POST":
+        data = request.form
+        # form_data=[]
+        # form_name = data.get('form-name')
+        # form_gender = data.get('form-gender')
+        # form_age = data.get('form-age')
+        # form_marital_status = data.get('form-marital-status')
+        # form_education_level = data.get('form-education-level')
+        # form_annual_income = data.get('form-annual-income')
+        # form_income_type = data.get('form-income_type')
+        # form_occupation = data.get('form-occupation')
+        # form_own_realty = data.get('form-own-realty')
+        # form_own_car = data.get('form-own-car')
+        # form_loan_type = data.get('form-loan-type')
+        # form_lona_period = data.get('form-lona-period')
+        # form_amount_annuity = data.get('form-amount-annuity')
+        # form_amount_credit = data.get('form-amount-credit') 
+       
+
+        # for key, value in data.items():
+        #   form_data += f"{key}: {value}\n"   
+        return render_template("scoring.html", data=data)
+
+    print(data.form_name)
+    
+@app.route('/scoring', methods=['POST'])
+def scoring():
+    form_data = request.form.get('form_data')
+    # Parse the form_data string to extract individual field values
+    # ... Process the data as needed
+    return render_template("scoring.html", form_data=form_data)
 
 
 
